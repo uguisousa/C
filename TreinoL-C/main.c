a51e3a642586f0d7b6d8457d5f70ebbe144e2d49
Guilherme Sousa <123672664+uguisousa@users.noreply.github.com>
2024-03-21 15:30:14 -0300

    Add files via upload

---
+++ TreinoL-C/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+int idade;
+
+printf("Digite sua idade.\nIdade: ");
+scanf("%d", &idade);
+if(idade >= 18){
+    printf("Você é maior de idade.");
+}
+else{
+    printf("Você é menor de idade");
+}
+}
