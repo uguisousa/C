85e16c5ef57d45334777c8d61a116654e62126fa
Guilherme Sousa <123672664+uguisousa@users.noreply.github.com>
2024-03-26 00:49:09 -0300

    Update mainerro.c

--- TreinoL-C/mainerro.c
+++ TreinoL-C/mainerro.c
@@ -1,1 +1,14 @@
-#include
+#include <stdio.h>
+
+int main(){
+int senha;
+
+printf("Digite sua idade.\nSenha ");
+scanf("%d", &idade);
+if(senha == 123){
+    printf("Senha correta");
+}
+else{
+    printf("Senha errada. ");
+}
+}
