a6e79348d8047181f8988499fab4a6f2ad20b63d
Guilherme Sousa <123672664+uguisousa@users.noreply.github.com>
2024-03-30 01:14:40 -0300

    Create treinoGdbOnline.c
    basico do basico em c 👍

---
+++ treinoGdbOnline.c
@@ -0,0 +1,50 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int opcao;
+    int senha;
+    int nome;
+    int idade;
+    
+    printf("   Seja Bem-vindo!! \n");
+    printf("Para efetuar o login, digite '1'. Se for novo na plataforma, digite '2'\n ");
+    
+    printf("Digite: ");
+    scanf("%d", &opcao);
+    if(opcao == 1){
+        printf("Digite a senha admin para efetuar o login\nSenha: ");
+        scanf("%d", &senha);
+        if(senha == 1243){
+            printf("Login efetuado com sucesso!");
+        }
+        else{
+            printf("Senha errada, volte e tente novamete!");
+        }
+    }
+    else if(opcao == 2){
+        printf("Para efetuar o cadastro, você precisara ser maior de idade!");
+        printf("Digite o seu nome: ");
+        scanf("%d", &nome);
+        if(nome )
+        
+        printf("Digite sua idade: ");
+        scanf("%d", &idade);
+        if(idade >= 18 ){
+            printf("Cadastro efetuado, parabens!");
+        }
+        else{
+            printf("Apenas de maiores podem fazer esse cadastro!");
+        }
+        
+    }
+    
+}
